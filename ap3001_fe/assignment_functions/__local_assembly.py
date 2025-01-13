e34558c7c473d55405bb5213e95c9835bea8168c
davy <davidvrchen@gmail.com>
2025-01-13 16:11:19 +0100

    did exercise 6; generate element mass matrix

--- ap3001_fe/assignment_functions/__local_assembly.py
+++ ap3001_fe/assignment_functions/__local_assembly.py
@@ -27,7 +27,7 @@ def generate_element_mass_matrix(basis, element_idx, sigma):
 
     for i_basis_idx in numpy.arange(0, 3):
         for j_basis_idx in numpy.arange(0, 3):
-            M_local[i_basis_idx, j_basis_idx] = 0
+            M_local[i_basis_idx, j_basis_idx] = w_quad @ (Bc_basis_eval[i_basis_idx, :] * Bc_basis_eval[j_basis_idx, :] * det_J)
 
     return M_local
 
@@ -104,7 +104,7 @@ def generate_element_vector(basis, element_idx, f):
     F_local = numpy.zeros(n_canonical_basis)  # pre-allocate memory for the local vector
 
     for j_basis_idx in numpy.arange(0, 3):
-        F_local[j_basis_idx] =  00
+        F_local[j_basis_idx] =  00 # TODO
 
     return F_local
 
